fd43b35e248b7ca251a491832295b2821548c165
Tejas K S <85862875+expertkidad@users.noreply.github.com>
2021-08-08 09:07:35 +0530

    Create precache-manifest.a2a56545174bfb07ec69778731e84c39.js

---
+++ precache-manifest.a2a56545174bfb07ec69778731e84c39.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7f6e331bb07070dc064cf3c691dd85",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "b4beeca99af7897bf5f5",
+    "url": "/sampleapp/static/css/2.25d0f266.chunk.css"
+  },
+  {
+    "revision": "a39b6dd02a4f1ab7376d",
+    "url": "/sampleapp/static/css/main.95fb0ab9.chunk.css"
+  },
+  {
+    "revision": "b4beeca99af7897bf5f5",
+    "url": "/sampleapp/static/js/2.42d8208c.chunk.js"
+  },
+  {
+    "revision": "ddfdb0a59d8e1d3b531d2e36b187b277",
+    "url": "/sampleapp/static/js/2.42d8208c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39b6dd02a4f1ab7376d",
+    "url": "/sampleapp/static/js/main.1b631f4c.chunk.js"
+  },
+  {
+    "revision": "277d2ba992b3a71ea751",
+    "url": "/sampleapp/static/js/runtime-main.345fd405.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/sampleapp/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/sampleapp/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/sampleapp/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/sampleapp/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/sampleapp/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
